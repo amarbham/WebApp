feb40aacdc5ff95038311735b506462f9f10fbc4
Amar <amarbham@hotmail.com>
2016-03-30 19:03:21 +0100

    Add gulp sourcemaps and minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 
 var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    cleanCSS = require('gulp-clean-css'),
     browserSync = require('browser-sync'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'), // TODO Remove after adding Browserify
+    concat = require('gulp-concat'), // TODO May remove after adding Browserify
     uglify = require('gulp-uglify'),
-    cleanCSS = require('gulp-clean-css');
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     util = require('gulp-util'),
@@ -27,25 +28,31 @@ var paths = {
     }
 }
 
-/* Custom Task Methods */
+/* Custom Task Configurations */
 var tasks = {
     styles: function() {
         return gulp.src([paths.src.sass])
             .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
             .pipe(autoprefixer('last 2 versions'))
+            .pipe(sourcemaps.write())
             .pipe(gulp.dest('./src/styles/'))
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest(paths.dist.styles))
             .pipe(browserSync.stream());
-
     },
     scripts: function() {
         return gulp.src([paths.src.app + '/app.module.js', paths.src.app + '/**/*.js'])
             .pipe(sourcemaps.init())
             .pipe(concat('main.js'))
-            .pipe(gulp.dest(paths.dist.scripts))
+            .pipe(gulp.dest('./src/app/'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('./src/app/'))
             .pipe(rename({ suffix: '.min' }))
             .pipe(uglify())
             .pipe(gulp.dest(paths.dist.scripts))
+
             .pipe(browserSync.stream());
     },
     templates: function() {
@@ -54,32 +61,44 @@ var tasks = {
             .pipe(gulp.dest(paths.dist.templates))
             .pipe(browserSync.stream());
     },
-    minifyCSS: function() {
-        return gulp.src(paths.src.styles + '**/*.css')
-            .pipe(sourcemaps.init())
-            .pipe(cleanCSS())
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(paths.dist.styles));
+    clean: {
+        scripts: function() {
+            return gulp.src('./src/app/main.js', { read: false })
+                .pipe(clean());
+        }
+    },
+    browserSync: function() {
+        return browserSync.init({
+            browser: ["google chrome"],
+            minify: false, /*** TODO Check if Dev build or production build and change accordingly */
+            injectChanges: true,
+            server: {
+                baseDir: "./"
+            },
+            port: process.env.PORT || 3000
+        })
     }
 }
 
+/*Custom Gulp Task declarations*/
+
+// Cleans javascript output main.js file
+gulp.task('clean-scripts', tasks.clean.scripts);
+
+// Transpiles SASS to CSS
 gulp.task('reload-styles', tasks.styles);
-gulp.task('reload-scripts', tasks.scripts);
+
+// Cleans any previous main.js output file first and then rebuilds all app scripts
+gulp.task('reload-scripts', ['clean-scripts'], tasks.scripts);
+
+// Rebuilds html templates
 gulp.task('reload-templates', tasks.templates);
 
-gulp.task('serve', ['reload-templates'], function() {
-    browserSync.init({
-        browser: ["google chrome"],
-        minify: false, /*** TODO Check if Dev build or production build and change accordingly */
-        injectChanges: true,
-        server: {
-            baseDir: "./dist"
-        },
-        port: process.env.PORT || 3000
-    });
-});
+// Builds html templates first and then fires up a local web server
+gulp.task('serve', ['reload-templates'], tasks.browserSync);
 
 
+//Watches changes in source files and reloads the browser on change
 gulp.task('watch', function() {
     gulp.watch(paths.src.html, ['reload-templates']).on('change', browserSync.reload)
     gulp.watch(paths.src.sass, ['reload-styles']).on('change', browserSync.reload)
@@ -87,13 +106,10 @@ gulp.task('watch', function() {
     util.log(util.colors.bgBlue('Watching for changes...'));
 });
 
-
 gulp.task('default', [
     'reload-templates',
     'reload-styles',
-    'reload-scripts',
-    'serve',
-    'watch'
+    'reload-scripts'
 ]);
 
 /*TODO Add dev, dev build and production build**/
---
+++ src/app/main.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        
+    ]);
+})();
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('componentController', componentController);
+
+    function componentController() {
+        var vm = this;
+            vm.show = "tfgfgfdgfdgdddt"
+        ////////////////
+    }
+})();
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5tb2R1bGUuanMiLCJjb21wb25lbnQvY29tcG9uZW50LmNvbnRyb2xsZXIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUNOQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24gKCkge1xyXG4gICAgJ3VzZSBzdHJpY3QnO1xyXG5cclxuICAgIGFuZ3VsYXIubW9kdWxlKCdhcHAnLCBbXHJcbiAgICAgICAgXHJcbiAgICBdKTtcclxufSkoKTsiLCIoZnVuY3Rpb24oKSB7XHJcbiAgICAndXNlIHN0cmljdCc7XHJcblxyXG4gICAgYW5ndWxhclxyXG4gICAgICAgIC5tb2R1bGUoJ2FwcCcpXHJcbiAgICAgICAgLmNvbnRyb2xsZXIoJ2NvbXBvbmVudENvbnRyb2xsZXInLCBjb21wb25lbnRDb250cm9sbGVyKTtcclxuXHJcbiAgICBmdW5jdGlvbiBjb21wb25lbnRDb250cm9sbGVyKCkge1xyXG4gICAgICAgIHZhciB2bSA9IHRoaXM7XHJcbiAgICAgICAgICAgIHZtLnNob3cgPSBcInRmZ2ZnZmRnZmRnZGRkdFwiXHJcbiAgICAgICAgLy8vLy8vLy8vLy8vLy8vL1xyXG4gICAgfVxyXG59KSgpO1xyXG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
